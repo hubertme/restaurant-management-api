6d4b2b966f740553ec63a6cf42575fe466353d03
Hubert Wang <hubert.tatra@gmail.com>
2022-05-27 19:18:44 +0700

    Add standard server responses

---
+++ responses/codes.ts
@@ -0,0 +1,10 @@
+export class SuccessCodes {
+    static OK = 'success/ok';
+}
+
+export class ErrorCodes {
+    static GENERAL_ERR = 'error/general';
+    
+    static UNIMPLEMENTED_METHOD = 'error/unimplemented-method';
+    static INVALID_INPUT = 'error/invalid-input';
+}
---
+++ responses/server_response.ts
@@ -0,0 +1,71 @@
+import {SuccessCodes, ErrorCodes} from './codes';
+
+class ResponseModel {
+    readonly code: string;
+    readonly message: string;
+    readonly data: any;
+
+    constructor(code: string, message: string = '', data: any = null) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
+
+export default class ServerResponse {
+    // --- GENERAL
+    static Success(data: any = null): ResponseModel {
+        return new ResponseModel(
+            SuccessCodes.OK,
+            'OK',
+            data
+        )
+    }
+
+    static Error(errorCode: string, message: string, data: any = null): ResponseModel {
+        if (message == '') {
+            message = 'Undefined error code...'
+        }
+
+        return new ResponseModel(
+            errorCode,
+            message,
+            data,
+        )
+    }
+
+    // --- SPECIFIC
+    static UnimplementedMethod(): ResponseModel {
+        return new ResponseModel(
+            ErrorCodes.UNIMPLEMENTED_METHOD,
+            'Non-existent endpoint',
+            null,
+        )
+    }
+
+    static ValidationError(message: string = 'Please check your body payload again'): ResponseModel {
+        return new ResponseModel(
+            ErrorCodes.INVALID_INPUT,
+            message,
+            null,
+        )
+    }
+
+    static GeneralError(data: any = null): ResponseModel {
+        if (data instanceof Error) {
+            data = {
+                error: {
+                    name: data.name,
+                    message: data.message,
+                    stack: data.stack,
+                }
+            };
+        }
+
+        return new ResponseModel(
+            ErrorCodes.GENERAL_ERR,
+            'An error occurred',
+            data,
+        )
+    }
+}
