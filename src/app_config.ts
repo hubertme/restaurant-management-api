2285232c0912449e8a1d6f7dad9f28fc35459677
Hubert Wang <hubert.tatra@gmail.com>
2022-05-27 18:37:13 +0700

    Add envs and AppConfig + environment init

---
+++ src/app_config.ts
@@ -0,0 +1,24 @@
+import * as path from "path";
+import { INestApplication } from "@nestjs/common";
+
+export default class AppConfig {
+    static readonly ENVS: 'dev' | 'staging' | 'prod' = 'dev';
+    static get isProduction(): boolean {
+        return this.ENVS === 'prod';
+    }
+    private static app: INestApplication;
+
+    public static init(app: INestApplication) {
+        this.app = app;
+        this.initDependencies();
+    }
+
+    static get envFilePath(): string {
+        const envPath = path.resolve(__dirname, `../envs/.${this.ENVS}.env`);
+        return envPath;
+    }
+
+    private static initDependencies() {
+        
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import AppConfig from './app_config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    AppConfig.init(app);
+
+    const port = process.env.PORT;
+    await app.listen(port, '0.0.0.0', () => {
+        if (!AppConfig.isProduction) {
+            console.log('Running on PORT:', port);
+        }
+    });
 }
 bootstrap();
